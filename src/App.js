a45487c465c1cb379b5408006856054d414d2886
Dillon Polley <65629239+ESSODillon@users.noreply.github.com>
2022-02-27 21:07:11 -0500

    React Router Dom version 6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+// Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 function App() {
-  return <div className="App">Dinner Dart</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <nav>
+          <h1>Dinner Dart</h1>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
