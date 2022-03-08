335aec8ad3f0ef61cde8fe887ab4142a700e46fb
Dillon Polley <65629239+ESSODillon@users.noreply.github.com>
2022-03-08 09:35:16 -0500

    Navbar, new pages, login functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 // Pages and Components
-import Home from "./pages/Home";
-import About from "./pages/About";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Cart from "./pages/cart/Cart";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const { user, authIsReady } = useAuthContext();
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -13,6 +19,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,76 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
+// Images
+import Tray from "../assets/food-tray.svg";
+import Bag from "../assets/shopping-bag.svg";
 
 export default function Navbar() {
+  const [activeNav, setActiveNav] = useState(false);
+
+  const toggleNav = () => {
+    if (activeNav) {
+      setActiveNav(false);
+    } else {
+      setActiveNav(true);
+    }
+  };
+
   return (
     <div className="navigation">
+      <input
+        type="checkbox"
+        className="navigation__checkbox"
+        id="navi-toggle"
+        checked={activeNav}
+        onChange={toggleNav}
+      ></input>
+
+      <label htmlFor="navi-toggle" className="navigation__button">
+        <span className="navigation__icon">&nbsp;</span>
+      </label>
+
+      <div className="navigation__background">&nbsp;</div>
+
+      <h1 className="heading-logo">
+        <img
+          src={Tray}
+          alt={"Dinner Dart logo"}
+          className="heading-logo--tray"
+        />
+        Dinner <span className="heading-logo--orange">Dart</span>
+      </h1>
+
       <nav className="navigation__nav">
-        <h1 className="heading-logo">
-          Dinner <span className="heading-logo--orange">Dart</span>
-        </h1>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+        <ul className="navigation__list">
+          <li className="navigation__item" id="navi-link" onClick={toggleNav}>
+            <Link to="/" className="navigation__link">
+              Home
+            </Link>
+          </li>
+          <li className="navigation__item" id="navi-link" onClick={toggleNav}>
+            <Link to="/about" className="navigation__link">
+              About
+            </Link>
+          </li>
+          <li className="navigation__item" id="navi-link" onClick={toggleNav}>
+            <Link to="/login" className="navigation__link">
+              Login
+            </Link>
+          </li>
+          <li className="navigation__item" id="navi-link" onClick={toggleNav}>
+            <Link to="/signup" className="navigation__link">
+              Signup
+            </Link>
+          </li>
+        </ul>
       </nav>
+
+      <Link to="/cart">
+        <div className="navigation__bag">
+          <img src={Bag} alt="Shopping bag" className="navigation__bag--icon" />
+        </div>
+      </Link>
     </div>
   );
 }
