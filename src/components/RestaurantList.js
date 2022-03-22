dbb71c31d5b6446f5fd2520a0336bd7088cdedbb
Dillon Polley <65629239+ESSODillon@users.noreply.github.com>
2022-03-22 01:00:55 -0400

    Delete functionality, card styling, details page routing

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -24,33 +24,48 @@ export default function RestaurantList({ restaurants }) {
   };
 
   return (
-    <Card>
+    <div className="restaurant-list">
       {restaurants.map((restaurant) => (
         <Card sx={{ maxWidth: 345 }} key={restaurant.id}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="140"
-              image={restaurant.image}
-              alt={restaurant.name}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {restaurant.name}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
-              <img
-                className="delete"
-                src={Trashcan}
-                onClick={() => deleteItem(restaurant.id)}
+          <Link
+            to={`/restaurants/${restaurant.id}`}
+            className="u-remove-text-decoration"
+          >
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image={restaurant.image}
+                alt={restaurant.name}
               />
-            </CardContent>
-          </CardActionArea>
+              <CardContent>
+                <Typography
+                  sx={{ fontSize: "1rem" }}
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  Word of the Day
+                </Typography>
+                <Typography gutterBottom variant="h5" component="div">
+                  {restaurant.name}
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  {restaurant.cuisines[0]}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lizards are a widespread group of squamate reptiles, with over
+                  6,000 species, ranging across all continents except Antarctica
+                </Typography>
+                <img
+                  className="delete"
+                  src={Trashcan}
+                  onClick={() => deleteItem(restaurant.id)}
+                />
+              </CardContent>
+            </CardActionArea>
+          </Link>
         </Card>
       ))}
-    </Card>
+    </div>
   );
 }
