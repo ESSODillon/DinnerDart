3494a85b68e870a9a3786d4b2637ef2b66671444
Dillon Polley <65629239+ESSODillon@users.noreply.github.com>
2022-03-31 21:27:31 -0400

    Menu List add Badge

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -14,8 +14,13 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
 import Box from "@mui/material/Box";
+import Badge from "@mui/material/Badge";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { createTheme } from "@mui/material/styles";
 
 export default function MenuList({ items }) {
   const { user } = useAuthContext();
@@ -23,7 +28,15 @@ export default function MenuList({ items }) {
   const { error, documents } = useCollection("users");
   const [isPending] = useState(false);
   const [admin, setAdmin] = useState(false);
-  console.log(items);
+  const [count, setCount] = React.useState(0);
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#d95d39",
+      },
+    },
+  });
 
   // If the currently logged user's ID matches a document in the users table ID, console log that users role
   useEffect(() => {
@@ -47,53 +60,77 @@ export default function MenuList({ items }) {
     <div className="menu--list">
       {items.length === 0 && <p>No items yet!</p>}
       {items.map((item) => (
-        <Card
-          sx={{
-            width: "45rem",
-            display: "flex",
-            justifyContent: "space-between",
-          }}
-          key={item.id}
-          style={{ fontSize: "1.6rem", fontFamily: "Lato" }}
-          className="menu--card"
-        >
-          <Box sx={{ display: "flex", flexDirection: "column" }}>
-            <CardContent>
-              <Typography sx={{ mb: 1.5 }} component="div" variant="h5">
-                {item.name}
-              </Typography>
+        <Badge fontSize="large" color="primary" badgeContent={count}>
+          <Card
+            sx={{
+              width: "45rem",
+              minHeight: "13rem",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+            key={item.id}
+            style={{ fontSize: "1.6rem", fontFamily: "Lato" }}
+            className="menu--card"
+          >
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <CardContent>
+                <Typography sx={{ mb: 1.5 }} component="div" variant="h5">
+                  {item.name}
+                </Typography>
+                <Typography
+                  variant="subtitle1"
+                  color="text.secondary"
+                  component="div"
+                >
+                  {item.description}
+                </Typography>
+
+                <ButtonGroup
+                  sx={{ position: "absolute", bottom: "1rem", right: "30%" }}
+                >
+                  <Button
+                    aria-label="increase"
+                    onClick={() => {
+                      setCount(count + 1);
+                    }}
+                  >
+                    <AddIcon fontSize="small" />
+                  </Button>
+                  <Button
+                    aria-label="reduce"
+                    onClick={() => {
+                      setCount(Math.max(count - 1, 0));
+                    }}
+                  >
+                    <RemoveIcon fontSize="small" />
+                  </Button>
+                </ButtonGroup>
+              </CardContent>
+
+              {admin && (
+                <img
+                  className="delete--menu-item"
+                  src={Trashcan}
+                  onClick={() => deleteItem(item.id)}
+                />
+              )}
+
               <Typography
                 variant="subtitle1"
-                color="text.secondary"
                 component="div"
+                className="menu--card__price"
               >
-                {item.description}
+                {item.price}
               </Typography>
-            </CardContent>
-
-            {admin && (
-              <img
-                className="delete--menu-item"
-                src={Trashcan}
-                onClick={() => deleteItem(item.id)}
-              />
-            )}
-
-            <Typography
-              variant="subtitle1"
-              component="div"
-              className="menu--card__price"
-            >
-              {item.price}
-            </Typography>
-          </Box>
-          <CardMedia
-            component="img"
-            sx={{ width: "25%" }}
-            image={item.image}
-            alt={item.name}
-          />
-        </Card>
+            </Box>
+            <CardMedia
+              component="img"
+              sx={{ width: "25%" }}
+              image={item.image}
+              alt={item.name}
+            />
+          </Card>
+        </Badge>
       ))}
     </div>
   );
