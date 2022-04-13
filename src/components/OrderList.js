2fab66700889d2305c080e678594098bf53b441f
Dillon Polley <65629239+ESSODillon@users.noreply.github.com>
2022-04-13 17:06:08 -0400

    More orders work

---
+++ src/components/OrderList.js
@@ -0,0 +1,6 @@
+// React, Firebase and Router
+import React, { useState } from "react";
+
+export default function OrderList({ items }) {
+  return <div className="orders--list">Display orders here</div>;
+}
--- src/pages/orders/Orders.js
+++ src/pages/orders/Orders.js
@@ -1,5 +1,31 @@
 import React from "react";
+import { useState } from "react";
+import { useCollection } from "../../hooks/useCollection";
+import OrderList from "../../components/OrderList";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { Typography } from "@mui/material";
 
 export default function Orders() {
-  return <div>Orders</div>;
+  const { user } = useAuthContext();
+  const [isPending, setIsPending] = useState(false);
+  const { documents, error } = useCollection("orders");
+
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+
+  if (!document) {
+    return <div className="loading">Loading...</div>;
+  }
+
+  return (
+    <div className="orders">
+      {/* {error && <p className="error">{error}</p>} */}
+      {isPending && <p className="loading">Loading...</p>}
+      <Typography sx={{ mb: 10, mt: 15, ml: "5rem" }} variant="h3">
+        Orders
+      </Typography>
+      {documents != null && <OrderList items={documents} />}
+    </div>
+  );
 }
